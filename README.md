077308d0b110e591e52e235c3e8fb4567f90b094
Yadnyesh Manohar Deore <116891797+Yadnyesh369@users.noreply.github.com>
2023-04-09 19:57:02 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-"# datastructure" 
+# DatastructureOperation
---
+++ dsMainClass.java
@@ -0,0 +1,13 @@
+package DataStructure;
+
+import HumanCloud.dynamicMainclassMethod;
+
+public class dsMainClass {
+
+	public static void main(String[] args) {
+		mainClass m1=new mainClass();
+		m1.MainMenu();
+	
+	}
+
+}
